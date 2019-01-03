8c4a3672d38b9eaf9bc39e2abfa278e7a5b77d33
Chris Perko <chris@chrisperko.net>
2019-01-03 18:17:29 -0500

    Displaying 24hr clock

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+import clock from "clock";
+import document from "document";
+
+clock.granularity = "minutes";
+clock.ontick = evt => {
+  let hours = evt.date.getHours();
+  let minutes = evt.date.getMinutes();
+
+  document.getElementById("clock").text = `${hours}:${minutes}`;
+};
