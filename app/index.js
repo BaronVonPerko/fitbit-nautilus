702f4b1478eb21acbf0d65e2d440ac7819bd372a
Chris Perko <chris@chrisperko.net>
2019-01-06 19:02:53 -0500

    New color theme for 'red'

--- app/index.js
+++ app/index.js
@@ -27,6 +27,11 @@ let themes = {
     main: "#0099ff",
     secondary: "#0040bb",
     gradient: "#000972"
+  },
+  red: {
+    main: "#ff1a1a",
+    secondary: "#cc1a1a",
+    gradient: "#661a1a"
   }
 };
 let currentTheme = "blue";
