0b6b2ab49bd0ff883cd1aa062b9514c469d79628
Chris Perko <chris@chrisperko.net>
2019-01-23 23:42:58 -0500

    Top left settings done

--- settings/index.jsx
+++ settings/index.jsx
@@ -14,7 +14,12 @@ function mySettings(props) {
           settingsKey="topLeft"
           options={[
             { name: "Steps", value: "getSteps" },
-            { name: "Heart Rate", value: "getHeartRate" }
+            { name: "Heart Rate", value: "getHeartRate" },
+            { name: "Floors", value: "getFloors" },
+            { name: "Active Minutes", value: "getActiveMinutes" },
+            { name: "Calories", value: "getCalories" },
+            { name: "Distance", value: "getDistance" },
+            { name: "Battery", value: "getPower" }
           ]}
         />
       </Section>
